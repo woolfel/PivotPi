a3bc9130a5929f9624f831834328cd64305d0c2c
Peter Lin <woolfel@gmail.com>
2017-09-23 08:12:11 -0400

    finally working on pivotpi drivers again. hopefully I have time tomorrow to test it

--- Software/java/src/org/dexind/pivotpi/PivotPi.java
+++ Software/java/src/org/dexind/pivotpi/PivotPi.java
@@ -1,10 +1,11 @@
 package org.dexind.pivotpi;
 
-import com.pi4j.io.i2c.I2CDevice;
 import com.pi4j.io.i2c.I2CFactory;
 import com.pi4j.io.i2c.I2CFactory.UnsupportedBusNumberException;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.pi4j.io.i2c.I2CBus;
 
@@ -27,23 +28,67 @@ public class PivotPi extends BasePCA9685 implements ServoController {
 		try {
 			this.bus = I2CFactory.getInstance(1);
 			this.i2cDevice = this.bus.getDevice(PCA9685.PCA9685_ADDRESS);
+			this.log = Logger.getLogger(PivotPi.class.getName());
 		} catch (UnsupportedBusNumberException e) {
 			e.printStackTrace();
+			log.log(Level.SEVERE, "Error instantiating PivotPi", e);
 		} catch (IOException e) {
 			e.printStackTrace();
+			log.log(Level.SEVERE, "Error instantiating PivotPi", e);
 		}
 	}
 	
 	@Override
 	public void angle(int channel, int angle) {
+		if (angle >= 0 && angle <= 180 && channel >= 0 && channel <= 7) {
+			int pwmToSend = 4095 - this.translate(angle, 0, 180, servo_min, servo_max);
+			try {
+				this.setPWM(channel, 0, pwmToSend);
+			} catch (IOException e) {
+				System.err.println(e.getMessage());
+			}
+		}
 	}
 
 	@Override
 	public void angleMicroseconds(int channel, int time) {
+		if (channel >= 0 && channel <= 7) {
+			try {
+				if (time <= 0) {
+					this.setPWM(channel, 4096, 4095);
+				} else {
+					int pwmToSend = 4095 - ((4096 / (1000000 / frequency)) * time);
+					if (pwmToSend < 0) {
+						pwmToSend = 0;
+					}
+					if (pwmToSend > 4096) {
+						pwmToSend = 4095;
+					}
+					this.setPWM(channel, 0, pwmToSend);
+				}
+			} catch (IOException e) {
+				
+			}
+		}
 	}
 
 	@Override
 	public void led(int channel, double percent) {
+		if (channel >= 0 && channel <= 7) {
+			try {
+				if (percent >= 100) {
+					this.setPWM(channel + 8, 4096, 4095);
+				} else {
+					if (percent < 0) {
+						percent = 0;
+					}
+					double pwmToSend = percent * 40.95d;
+					this.setPWM(channel +8, 0, (int)pwmToSend);
+				}
+			} catch (IOException e) {
+				
+			}
+		}
 	}
 
 	@Override
@@ -57,6 +102,19 @@ public class PivotPi extends BasePCA9685 implements ServoController {
 
 	    // Convert the 0-1 range into a value in the right range.
 	    return (int)(rightMin + (valueScaled * rightSpan));
+	}
+	
+	public void setPWM(int channel, int on, int off) throws IOException {
+
+		Integer onl = new Integer(on & 0xFF);
+		Integer onh = new Integer(on >> 8);
+		Integer offl = new Integer(off & 0xFF);
+		Integer offh = new Integer(off >> 8);
+		
+		this.i2cDevice.write(LED0_ON_L+4*channel, onl.byteValue());
+		this.i2cDevice.write(LED0_ON_H+4*channel, onh.byteValue());
+		this.i2cDevice.write(LED0_OFF_L+4*channel, offl.byteValue());
+		this.i2cDevice.write(LED0_OFF_H+4*channel, offh.byteValue());
 	}
 
 }
